7287e9e82c5d45dc1ab5953fddab2c1c7eb9963c
JorgeHdzSoria <jorge.hdzsoria@gmail.com>
2022-10-11 22:11:25 -0500

    Corrected

--- src/oop/parcial2/neighborhood/StreetBuilder.java
+++ src/oop/parcial2/neighborhood/StreetBuilder.java
@@ -1,37 +1,31 @@
 package oop.parcial2.neighborhood;
 
-import com.sun.source.tree.Tree;
-
 import java.util.TreeMap;
 
-public class StreetBuilder
-{
+public class StreetBuilder {
     private String name;
     private TreeMap<Integer, House> houses;
 
-    public StreetBuilder name(String name)
-    {
+    public StreetBuilder(){
+        houses = new TreeMap<>();
+    }
+
+    public StreetBuilder name(String name) {
         this.name = name;
-
         return this;
     }
 
-    public StreetBuilder houses(TreeMap<Integer, House> houses)
-    {
+    public StreetBuilder houses(TreeMap<Integer, House>  houses) {
         this.houses = houses;
-
         return this;
     }
 
-    public StreetBuilder house(int number, House house)
-    {
-        houses.put(number, house);
-
+    public StreetBuilder house(int number, House  house) {
+        houses.put(number,house);
         return this;
     }
 
-    public Street build()
-    {
-        return new Street(name,houses);
+    public Street build(){
+        return new Street(name, houses);
     }
 }
